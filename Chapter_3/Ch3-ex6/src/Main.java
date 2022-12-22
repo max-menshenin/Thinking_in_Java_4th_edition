ab00d260e4402fb5c4e40e4bfc0f1657a754adbc
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-12-22 18:06:27 +0300

    test

--- Chapter_3/Ch3-ex6/src/Main.java
+++ Chapter_3/Ch3-ex6/src/Main.java
@@ -1,4 +1,15 @@
-/*
-Exercise 6: (3) Following Exercise 5, create a new Dog reference and assign it to spot’s object.
- Test for comparison using == and equals( ) for all references.
- */
+/* Exercise 6: (3) Following Exercise 5, create a new Dog reference and assign it to spot’s object.
+ Test for comparison using == and equals( ) for all references.*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        dog1.name = "spot";
+        dog1.says = "Ruff!";
+        dog2.name = "scruffy";
+        dog2.says = "Wurf";
+
+    }
+}
