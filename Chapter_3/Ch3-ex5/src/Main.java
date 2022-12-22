1cf0e865161cd8943b885761aa0e27dca6254a6a
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-12-22 15:47:09 +0300

    added ex6

--- Chapter_3/Ch3-ex5/src/Main.java
+++ Chapter_3/Ch3-ex5/src/Main.java
@@ -1,7 +1,20 @@
 /* Exercise 5: (2) Create a class called Dog containing two Strings:
 name and says. In main( ), create two dog objects with names “spot” (who says, “Ruff!”) and “scruffy” (who says, “Wurf!”). Then display their names and what they say. */
+
+class Dog {
+    String name;
+    String says;
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        dog1.name = "spot";
+        dog1.says = "Ruff!";
+        dog2.name = "scruffy";
+        dog2.says = "Wurf";
+
+        System.out.println(dog1.name + " says " + dog1.says);
+        System.out.println(dog2.name + " says " + dog2.says);
     }
 }
