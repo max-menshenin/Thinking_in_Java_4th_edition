8a304aec55ef5296910661473aec648ec8e6f7c0
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-12-24 11:16:54 +0300

    little fix

--- Chapter_03/Ch3-ex6/src/Main.java
+++ Chapter_03/Ch3-ex6/src/Main.java
@@ -8,12 +8,12 @@ class Dog {
 }
 public class Main {
     static void compare(Dog dog1,Dog dog2) {
-        System.out.println("== on top references: "+(dog1==dog2));
-        System.out.println(".equals() on top references: "+dog1.equals(dog2));
-        System.out.println("== on name: "+(dog1.name==dog2.name));
-        System.out.println(".equals() on name: "+dog1.name.equals(dog2.name));
-        System.out.println("== on Says: "+(dog1.says==dog2.says));
-        System.out.println(".equals() on Says: "+dog1.says.equals(dog2.says));
+        System.out.println("== on top references: " + (dog1 == dog2));
+        System.out.println(".equals() on top references: " + dog1.equals(dog2));
+        System.out.println("== on name: "+(dog1.name == dog2.name));
+        System.out.println(".equals() on name: " + dog1.name.equals(dog2.name));
+        System.out.println("== on Says: "+(dog1.says == dog2.says));
+        System.out.println(".equals() on Says: " + dog1.says.equals(dog2.says));
     }
     public static void main(String[] args) {
         Dog dog1 = new Dog();
