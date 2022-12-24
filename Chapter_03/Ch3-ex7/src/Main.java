afb34eb19417b7700fb4c22ce902279881e02126
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-12-24 11:37:55 +0300

    fix task

--- Chapter_03/Ch3-ex7/src/Main.java
+++ Chapter_03/Ch3-ex7/src/Main.java
@@ -1,5 +1,5 @@
-/* Exercise 6: (3) Following Exercise 5, create a new Dog reference and assign it 
-to spot’s object. Test for comparison using == and equals( ) for all references.*/
+/* Exercise 5: (2) Create a class called Dog containing two Strings:
+name and says. In main( ), create two dog objects with names “spot” (who says, “Ruff!”) and “scruffy” (who says, “Wurf!”). Then display their names and what they say. */
 
 
 class Dog {
