c45b953632678975b74467cc725ffd4ef9581355
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-12-24 11:39:30 +0300

    corrected new line

--- Chapter_03/Ch3-ex6/src/Main.java
+++ Chapter_03/Ch3-ex6/src/Main.java
@@ -1,7 +1,6 @@
 /* Exercise 6: (3) Following Exercise 5, create a new Dog reference and assign it 
 to spot’s object. Test for comparison using == and equals( ) for all references.*/
 
-
 class Dog {
     String name;
     String says;
