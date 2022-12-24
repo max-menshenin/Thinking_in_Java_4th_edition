fcbf26fed396b8fdb353ad7f1c863b29891f8119
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-12-24 14:41:17 +0300

    fixed

--- Chapter_03/Ch3-ex7/src/Main.java
+++ Chapter_03/Ch3-ex7/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Random flip = new Random();
         flip.nextBoolean();
-        System.out.println(flip.nextBoolean() == true?"Head":"Tail");    }
+        System.out.println(flip.nextBoolean() == true?"Head":"Tail");
+    }
 }
