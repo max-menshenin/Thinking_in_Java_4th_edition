1d1972214613556090ed3700aa7420215d1f4201
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-12-26 12:53:41 +0300

    added ex10

--- Chapter_03/Ch3-ex10/src/Main.java
+++ Chapter_03/Ch3-ex10/src/Main.java
@@ -8,28 +8,27 @@ public class Main {
     public static void main(String[] args) {
         int i1 = 0Xa3355fcd;
         int i2 = 0Xec355edd;
-        System.out.println("i1 = "+Integer.toBinaryString(i1));
-        System.out.println("i2 = "+Integer.toBinaryString(i2));
-        System.out.println("~i1 =  "+Long.toBinaryString(~i1));
-        System.out.println("~i2 = "+Long.toBinaryString(~i2));
-        System.out.println("i1&i1 =  "+Long.toBinaryString(i1&i1));
-        System.out.println("i1|i1 =  "+Long.toBinaryString(i1|i1));
-        System.out.println("i1^i1 =  "+Long.toBinaryString(i1^i1));
-        System.out.println("i1&i2 =  "+Long.toBinaryString(i1&i2));
-        System.out.println("i1|i2 =  "+Long.toBinaryString(i1|i2));
-        System.out.println("i1^i2 =  "+Long.toBinaryString(i1^i2));
+        System.out.println("i1 = " + Integer.toBinaryString(i1));
+        System.out.println("i2 = " + Integer.toBinaryString(i2));
+        System.out.println("~i1 =  " + Long.toBinaryString(~i1));
+        System.out.println("~i2 = " + Long.toBinaryString(~i2));
+        System.out.println("i1&i1 =  " + Long.toBinaryString(i1 & i1));
+        System.out.println("i1|i1 =  " + Long.toBinaryString(i1 | i1));
+        System.out.println("i1^i1 =  " + Long.toBinaryString(i1 ^ i1));
+        System.out.println("i1&i2 =  " + Long.toBinaryString(i1 & i2));
+        System.out.println("i1|i2 =  " + Long.toBinaryString(i1 | i2));
+        System.out.println("i1^i2 =  " + Long.toBinaryString(i1 ^ i2));
     }
 }
 
 // Out:
-//  i1 = 10100011001101010101111111001101
-//  i2 = 11101100001101010101111011011101
-//  ~i1 =  1011100110010101010000000110010
-//  ~i2 = 10011110010101010000100100010
-//  i1&i1 =  1111111111111111111111111111111110100011001101010101111111001101
-//  i1|i1 =  1111111111111111111111111111111110100011001101010101111111001101
-//  i1^i1 =  0
-//  i1&i2 =  1111111111111111111111111111111110100000001101010101111011001101
-//  i1|i2 =  1111111111111111111111111111111111101111001101010101111111011101
-//  i1^i2 =  1001111000000000000000100010000
-
+// i1 = 10100011001101010101111111001101
+// i2 = 11101100001101010101111011011101
+// ~i1 = 1011100110010101010000000110010
+// ~i2 = 10011110010101010000100100010
+// i1&i1 = 1111111111111111111111111111111110100011001101010101111111001101
+// i1|i1 = 1111111111111111111111111111111110100011001101010101111111001101
+// i1^i1 = 0
+// i1&i2 = 1111111111111111111111111111111110100000001101010101111011001101
+// i1|i2 = 1111111111111111111111111111111111101111001101010101111111011101
+// i1^i2 = 1001111000000000000000100010000
