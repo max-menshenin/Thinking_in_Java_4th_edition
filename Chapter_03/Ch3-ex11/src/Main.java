20bbc3e7db1eb05a08fd94da430c207305fa5975
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-01-17 16:50:34 +0300

    init

--- Chapter_03/Ch3-ex11/src/Main.java
+++ Chapter_03/Ch3-ex11/src/Main.java
@@ -3,7 +3,8 @@
   of its binary positions, each time displaying the result using Integer.toBinaryString( ) */
   
 public class Main {
-    public static void main(String[] args) { int h = 0x10000000;
+    public static void main(String[] args) { 
+        int h = 0x10000000;
         System.out.println(Integer.toBinaryString(h));
         for (int k = 0; k < 28; k++) {
             h >>>= 1;
